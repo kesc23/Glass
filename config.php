e07e2b02647a3237cf1f6fd354fc47529034aaa3
Kevin Campos <kevincampos.2475@aluno.una.br>
2021-08-23 15:24:47 -0300

    Update config.php
    - Edited phpdocs
    - Added a verifier to GLASS_* constants in case a program using glass just edit them to perform a core task and etc.

--- config.php
+++ config.php
@@ -11,11 +11,16 @@ define( 'GLASS_DEBUG',
 
 /**
  * Define constants for paths inside Glass
+ * 
+ * GLASS_DIR      Define Root Path
+ * INCLUDES       Define Includes Path
+ * CLASSES        Define Classes path
+ * PLUGINS_DIR    Define Classes path
  */
-define( 'GLASS_DIR', __DIR__ . '/' );             // Define Root Path
-define( 'INCLUDES', GLASS_DIR . 'includes/');     // Define Includes Path
-define( 'CLASSES', INCLUDES . 'class/' );         // Define Classes path
-define( 'PLUGINS_DIR', GLASS_DIR . 'plugins/' );  // Define Classes path
+define( 'GLASS_DIR', __DIR__ . '/' ); 
+define( 'INCLUDES', GLASS_DIR . 'includes/');
+define( 'CLASSES', INCLUDES . 'class/' ); 
+define( 'PLUGINS_DIR', GLASS_DIR . 'plugins/' );
 
 
 /**
@@ -26,11 +31,25 @@ define( 'GLASS_CLASSES', true );
 
 /**
  * Define the constants for the Glass Database:
- * Insert them as in your server.
+ * Insert them as in your server or configure them in another file to be included before.
+ * 
+ * GLASS_HOST        is the mysql host with port to PDO/mysqli access. schema: server:port
+ * GLASS_HOST_USER   is the mysql user admin.
+ * GLASS_HOST_PASS   is the mysql user password.
+ * GLASS_DB          is the mysql database that the data will be stored.
+ * GLASS_SSL         For SSL Conn. Set to true to enable VERIFY_PEERS in APIConsummer cURL requests.
  */
-define( 'GLASS_HOST', '127.1.2.3:3306' );
-define( 'GLASS_HOST_USER', 'root' );
-define( 'GLASS_HOST_PASS', '' );
-define( 'GLASS_DB', 'glass' );
+$_constants = array(
+    'GLASS_HOST'      => '127.1.2.3:3306',
+    'GLASS_HOST_USER' => 'root',
+    'GLASS_HOST_PASS' => '',
+    'GLASS_DB'        => 'glass',
+    'GLASS_SSL'       => false,
+);
+
+foreach( $_constants as $constant => $value )
+{
+    if( ! defined( $constant ) ): define( $constant, $value ); endif;
+}
 
 require_once 'CoreLoader.php';
